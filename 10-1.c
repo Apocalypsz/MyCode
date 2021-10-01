e07a1339a86498776b4c9310b5c5bd13b4090509
Apocalypsz <lihaotian189@163.com>
2021-10-02 00:37:03 +0800

    Create 10-1.c

---
+++ 10-1.c
@@ -0,0 +1,165 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+ 
+int main()
+{
+	//int day;
+	//scanf("%d", &day);
+	//switch (day)
+	//{
+	//case 1:
+	//	printf("周一");
+	//	break;
+	////........
+	//default:
+	//	printf("请输入1-7的数字");
+	//	break;
+
+	//int	n = 1;
+	//int m = 2;
+	//switch (n)
+	//{
+	//case 1:
+	//	m++;
+	//case 2:
+	//	n++;
+	//case 3:
+	//	switch (n)
+	//	{
+	//	case 1:
+	//		n++;
+	//	case 2:
+	//		m++; 
+	//		n++;
+	//		break;
+	//	}
+	//	break;
+	//case 4:
+	//	m++;
+	//	break;
+	//default:
+	//	break;
+	//}
+	//printf("m = %d, n = %d", m, n);
+
+	//char password[20] = { 0 };
+	//printf("请输入密码:");
+	//scanf("%s", password);
+	//printf("请确认密码(Y/N):");
+	//int temp = 0;
+	//while ((temp = getchar()) != '\n')
+	//{
+	//	;
+	//}
+	//
+	//int ch = getchar();
+	//if (ch == 'Y')
+	//	printf("确认成功");
+	//else
+	//	printf("确认失败");
+
+	//①计算n的阶乘
+	//int a = 0;
+	//int i = 0;
+	//scanf("%d", &a);
+	//int ans = 1;
+	//do 
+	//{
+	//	i++;
+	//	ans = ans * i;
+	//} while (i < a);
+	//	
+	//printf("%d", ans);
+
+	//int a;
+	//int i;
+	//int ans = 1;
+	//scanf("%d", &a);
+	//for (i = 1; i < a+1; i++)
+	//{
+	//	ans *= i;
+	//}
+	//printf("%d", ans);
+
+	/*int a;
+	int ans = 1;
+	int i = 1;
+	scanf("%d", &a);
+	while (i < a+1)
+	{
+		ans *= i;
+		i++;
+	}
+	printf("%d", ans);*/
+
+	//②计算1!+2!+....+10!
+	//int sum = 0;
+	//int i;
+	//int j;
+	//for (i = 0; i < 2; i++)
+	//{
+	//	int jie = 1;
+	//	for (j = 1; j < i + 2; j++)
+	//	{
+	//		jie *= j;
+	//	}
+	//	sum += jie;
+	//}
+	//printf("%d", sum);
+
+	//③
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	//④多个字符从两端移动向中间汇聚
+	//int a = 20;
+	//int b = 0;
+	//int i = 0;
+	//int c;
+	//while (i < a)
+	//{
+	//	
+	//	for (c = 0; c < i; c++)//打印前面的点
+	//	{
+	//		printf(".");
+	//	}
+	//	printf("abc");//打印前面字符
+	//	for (c = 1; c < 2*(a - i)-1; c++)//打印中间的点
+	//	{
+	//		printf(".");
+	//	}
+	//	printf("abc");//打印后面字符
+	//	for (c = 0; c < i; c++)//打印后面的点
+	//	{
+	//		printf(".");
+	//	}
+
+	//	printf("\n");
+	//	i++;
+	}
+
+
+	return 0;
+}
