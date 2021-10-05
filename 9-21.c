c0fdd8248aec506b2e894ea973629c99f041a3ee
Apocalypsz <lihaotian189@163.com>
2021-10-05 23:23:39 +0800

    Create 9-21.c

---
+++ 9-21.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+
+
+
+int main()
+{
+	/*int i = 0;
+	int seq[] = { 1,2,3,4,5,6,7,8,9,10 };
+	while (i < 10)
+	{
+		printf("%d, ", seq[i]);
+		i++;
+
+	}*/
+
+	/*int a = 2;
+	int b = a << 9;
+	printf("%d\n", b);*/
+
+	/*int a = -4;
+	int b = 10;
+	if (a || b == 10)
+	{
+		printf("dui");
+	}
+	else
+	{
+		printf("cuo");
+	}*/
+
+	int aaa[10] = { 0 };
+	int i = 0;
+	while (i < 10) {
+		printf("%d\n", aaa[i]);
+		i++;
+	}
+
+
+	
+
+
+
+
+
+
+
+
+}
