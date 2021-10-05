40dfd38b5e2f31a17809fd5a8a153cd8bb9a7d82
Apocalypsz <lihaotian189@163.com>
2021-10-05 23:23:34 +0800

    Create 10-5.c

---
+++ 10-5.c
@@ -0,0 +1,70 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+//void menu()
+//{
+//	printf("*********************************\n");
+//	printf("************  1.play   **********\n");
+//	printf("************  0.exit   **********\n");
+//	printf("*********************************\n");
+//}
+//
+//void game()//实现猜数字
+//{
+//	int ret = rand() %100+1;//100的余数是0-99，+1即为1-100
+//	int gus = 0;
+//	while (1)
+//	{
+//		printf("猜数字:>");
+//		scanf("%d", &gus);
+//		if (gus == ret)
+//		{
+//			printf("猜对了\n");
+//			break;
+//		}
+//		else if (gus > ret)
+//		{
+//			printf("猜大了\n");
+//		}
+//		else
+//		{
+//			printf("猜小了\n");
+//		}
+//	}
+//}
+//
+//int main()
+//{
+//	srand((unsigned int)time(NULL));
+//	int input = 0;
+//	do
+//	{
+//		menu();//打印菜单
+//		printf("请选择：>");
+//		scanf("%d", &input);
+//		switch (input)
+//		{
+//		case 1:
+//			game();
+//			break;
+//		case 0:
+//			printf("退出游戏\n");
+//			break;
+//		default:
+//			printf("选择错误，重新选择\n");
+//			break;
+//		}
+//	} while (input);
+
+
+
+
+
+
+
+
+	return 0;
+}
