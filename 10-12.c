9480bf3851ea537d5cc7bd64118638904276d5d8
Apocalypsz <lihaotian189@163.com>
2021-10-12 22:50:11 +0800

    Create 10-12.c

---
+++ 10-12.c
@@ -0,0 +1,67 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+//不用临时变量求字符串长度
+//int mystrlen(char* str)
+//{
+//	if (*str != '\0')
+//	{
+//		return 1 + mystrlen(str + 1);
+//	}
+//	else
+//	{
+//		return 0;
+//	}
+//}
+
+//求斐波那契数
+//int fib(int n)
+//{
+//	if (n <= 2)
+//	{
+//		return 1;
+//	}
+//	else
+//	{
+//		return fib(n - 1) + fib(n - 2);
+//	}
+//
+//}
+
+//打印乘法表
+//void printtable(int a)
+//{
+//	int i = 0;
+//	for (i = 1; i <= a; i++)
+//	{
+//		int j = 0;
+//		for (j = 1; j <= a; j++) 
+//		{
+//			printf("%d * %d = %2d  ", i, j, i * j);
+//		}
+//		printf("\n");
+//	}
+//
+//}
+
+int main()
+{
+	////不用临时变量求字符串长度
+	//char arr[] = "bit";
+	//printf("%d\n", mystrlen(arr));
+
+	//求斐波那契数
+	//int n = 0;
+	//scanf("%d", &n);
+	//int ret = fib(n);
+	//printf("%d\n", ret);
+
+	//打印乘法表
+	//int a = 0;
+	//scanf("%d", &a);
+	//printtable(a);
+
+
+
+
+	return 0;
+}
