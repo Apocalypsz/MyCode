63bf984f8deaa149d1ce8431e0ff56efba69e8d8
Apocalypsz <lihaotian189@163.com>
2021-10-13 21:19:45 +0800

    Create 10-13.c

---
+++ 10-13.c
@@ -0,0 +1,32 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+
+//求一个数每一位上的数字之和
+//int digitsum(int  n)
+//{
+//	if (n >9)
+//	{
+//		return digitsum(n / 10) + n % 10;
+//	}
+//	else
+//	{
+//		return n;
+//	}
+//}
+
+
+
+int main()
+{
+	////求一个数每一位上的数字之和
+	//int num = 1200;
+	//int sum = digitsum(num);
+	//printf("%d", sum);
+
+
+
+
+
+	return 0;
+}
