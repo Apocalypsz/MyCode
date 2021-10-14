b7c312e0b8237991768962e58d8ead395f414669
Apocalypsz <lihaotian189@163.com>
2021-10-14 22:54:48 +0800

    Update game.c

--- sanziqi/game.c
+++ sanziqi/game.c
@@ -1,4 +1,51 @@
-#pragma once
+#include "game.h"
 
-#define ROW 3
-#define COL 3
+
+
+void InitBoard(char board[ROW][COL], int row, int col)
+{
+	int i = 0;
+	int j = 0;
+	for (i = 0; i < row; i++)
+	{
+		for (j = 0; j < col; j++)
+		{
+			board[i][j] = ' ';
+		}
+	}
+}
+
+void DisplayBoard(char board[ROW][COL], int row, int col)
+{
+	int i = 0;
+	for (i = 0; i < row; i++)
+	{
+		//打印棋子行
+		int j = 0;
+		for (j = 0; j < col; j++)
+		{
+			printf(" %c ", board[i][j]);
+			if (j < col - 1)
+			{
+				printf("|");
+			}
+		}
+		printf("\n");
+
+		//打印间隔行
+		if (i < row-1)
+		{
+			for (j = 0; j < col; j++)
+			{
+				printf("---", board[i][j]);
+				if (j < col - 1)
+				{
+					printf("|");
+				}
+			}
+			printf("\n");
+		}
+
+	}
+
+}
