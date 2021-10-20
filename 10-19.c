6d4a825e1b89cbf66c1f80adcb418a7f6c92a395
Apocalypsz <lihaotian189@163.com>
2021-10-20 22:53:51 +0800

    Create 10-19.c

---
+++ 10-19.c
@@ -0,0 +1,32 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+int main()
+{
+	//int a = 10;
+	//int* pa = &a;
+	//char ch = 'w';
+	//char* pch = &ch;
+	//double* d[5];
+	//double*(*pd)[5] = &d;//pd即是一个数组指针（数组指针是一个指针）
+
+	//int arr[10] = { 1,2,3,4,5 };
+	//int(*parr)[10] = &arr;//取出数组的地址
+	////parr是一个数组指针，存放的是数组的地址
+
+	//函数的指针*pt
+	//void test(char* str)
+	//{
+	// ;
+	//}
+	//void (*pt)(char*)=&test;
+
+
+
+
+
+
+
+
+	return 0;
+}
